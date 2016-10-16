47dce275bfa2e030f1f5b0a64eb8920ff8ed0528
para7 <minstrel.suppin@gmail.com>
2016-10-16 18:28:17 +0900

    機能を関数化する第一歩

---
+++ mySDL.hpp
@@ -0,0 +1,13 @@
+#ifndef mySDL_hpp
+#define mySDL_hpp
+
+namespace mySDL
+{
+    void SystemInit(double w_rate);
+    bool Update();
+    void DrawLine(int x, int y, int x2, int y2);
+    
+    void Quit();
+}
+
+#endif /* mySDL_hpp */
