7c1caeeb48089028a21a9b37ceb433426ca44498
para7 <minstrel.suppin@gmail.com>
2017-01-30 00:19:42 +0900

    add DrawText

---
+++ SDL practice/mySDL/DrawText.cpp
@@ -0,0 +1,9 @@
+//
+//  Draw Text.cpp
+//  SDL practice
+//
+//  Created by yuta ishii on 2017/01/30.
+//  Copyright © 2017年 para7. All rights reserved.
+//
+
+#include "Draw Text.hpp"
---
+++ SDL practice/mySDL/DrawText.hpp
@@ -0,0 +1,14 @@
+//
+//  Draw Text.hpp
+//  SDL practice
+//
+//  Created by yuta ishii on 2017/01/30.
+//  Copyright © 2017年 para7. All rights reserved.
+//
+
+#ifndef Draw_Text_hpp
+#define Draw_Text_hpp
+
+#include <stdio.h>
+
+#endif /* Draw_Text_hpp */
