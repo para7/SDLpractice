e1fe81883e7ca220608f6499b59999ac5cde6f6e
para7 <minstrel.suppin@gmail.com>
2017-02-28 17:47:03 +0900

    まーじようコミット

--- SDL practice/mySDL/DrawText.cpp
+++ SDL practice/mySDL/DrawText.cpp
@@ -4,7 +4,6 @@
 #include "Make structs.hpp"
 #include "Define.hpp"
 #include <string>
-
 #include SDL2_path
 
 namespace mySDL
@@ -19,11 +18,7 @@ namespace mySDL
         SDL_Rect drawrect = {0,0,50,50};
         
 
-        if(texttex == nullptr)
-        {
-            printf("call CreateTexture¥n");
-            texttex = SDL_CreateTexture(renderer,         SDL_PIXELFORMAT_ARGB8888,SDL_TEXTUREACCESS_STREAMING,640, 480);
-        }
+        
         if(textsur == nullptr)
         {
             textsur            = SDL_CreateRGBSurface(0, 640, 480, 32,0x00FF0000,0x0000FF00,0x000000FF,0xFF000000);
@@ -39,23 +34,31 @@ namespace mySDL
             SDL_FreeSurface(textsur);
             return;
         }
+        int w,h;
+        
+        textsur = TTF_RenderUTF8_Blended(font, str, color);
+        if(texttex == nullptr)
+        {
+             texttex = SDL_CreateTexture(renderer,         SDL_PIXELFORMAT_ARGB8888,SDL_TEXTUREACCESS_STREAMING,textsur->w, textsur->h);
+        }
+        drawrect.w = textsur->w;
+        drawrect.h = textsur->h;
+        SDL_UpdateTexture(texttex, &drawrect, textsur->pixels, textsur->pitch*textsur->h);
+        
+        SDL_QueryTexture(texttex, NULL, NULL, &w, &h);
+        drawrect = makeRect(x,y,textsur->w,textsur->h);
+        SDL_RenderCopy(renderer, texttex, NULL, NULL);
+/*
         textsur = TTF_RenderUTF8_Blended(font, str, color);
         drawrect = makeRect(0,0,textsur->w,textsur->h);
         SDL_UpdateTexture(texttex, &drawrect, textsur->pixels, textsur->pitch);
         SDL_RenderCopy(renderer, texttex, NULL, &drawrect);
-        
+        */
         TTF_CloseFont(font);
         SDL_FreeSurface(textsur);
-//        textsur = nullptr;
         SDL_DestroyTexture(texttex);
-        
     }
-/*
-    void DrawText(const int x,const int y,const int size ,SDL_Color &color ,const std::string &str ,const char *fontname)
-    {
-        DrawText(x, y, size, color, str.c_str(),fontname);
-        return;
-    }
-*/
+
+    
     
 }
